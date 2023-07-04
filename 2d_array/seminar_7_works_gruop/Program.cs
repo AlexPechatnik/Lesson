52dcca4af59f7f0898cff76de5c8747c09d424e2
Alexander Kalugin <cg.alexander.kalugin@gmail.com>
2023-07-04 17:52:12 +0300

    add_seminar_7

---
+++ 2d_array/seminar_7_works_gruop/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+*/
+int m = 3;
+int n = 4;
+
+int[,] GetFillAmnMatrix(int width, int height)
+{
+    int[,] new_array = new int[width, height];
+
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            new_array[i, j] = i + j;
+        }
+    }
+    return new_array;
+}
+
+void PrintAmnMatrix (int[,] imputMatrix)
+{
+    for (int i = 0; i < imputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < imputMatrix.GetLength(1); j++)
+        {
+            Console.WriteLine(imputMatrix[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+PrintAmnMatrix(GetFillAmnMatrix(m, n));
