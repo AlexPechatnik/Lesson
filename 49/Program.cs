00a9496a654446224fa2cebb727ed3d54e96bc9f
Alexander Kalugin <cg.alexander.kalugin@gmail.com>
2023-07-05 18:00:33 +0300

    add_05.07

--- 49/Program.cs
+++ 49/Program.cs
@@ -1,8 +1,24 @@
 ﻿/*Задача 49: Задайте двумерный массив. Найдите элементы, 
 у которых оба индекса чётные, и замените эти элементы на их квадраты.
 */
-int[,] array = new int[10,10];
-for (int i = 0; i < length; i++)
+
+int[,] array = new int[10, 10];
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 10); ;
+        System.Console.Write(array[i, j] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+for (int i = 0; i < array.GetLength(1); i++)
+{
+    double sum = 0;
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+       sum = sum + array[i, j];
+    }
+    System.Console.WriteLine($"Средняя ариф. строки {i}: {sum / array.GetLength(1)}");
 }
---
+++ Lesson_8_exampls_53/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 53: Задайте двумерный массив. Напишите программу, 
+которая поменяет местами первую и последнюю строку массива.
+*/
+
+int[,] array = new int[5, 5];
+int[,] temp = new int[5, 5];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 10); ;
+        System.Console.Write(array[i, j] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = temp[i, j];
+        System.Console.Write(temp[i, j]);
+    }
+    System.Console.Write(" ");
+}
