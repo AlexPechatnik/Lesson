41f6147bb3f8811fd47613ddb20e07348086627e
Alexander Kalugin <cg.alexander.kalugin@gmail.com>
2023-07-10 11:10:15 +0300

    add_59

--- Lesson_8_exampls_59/Program.cs
+++ Lesson_8_exampls_59/Program.cs
@@ -1,24 +1,43 @@
-﻿/* Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+﻿/* Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, 
+которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
 */
-int[,] array = new int[5, 5];
-int [,] point = new int [array.GetLength(0), array.GetLength(1)];
-int min = 0;
+int[,] array = new int[3, 3];
+int minRow = 0;
+int minCol = 0;
 
 for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i, j] = new Random().Next(0, 9);
+        array[i, j] = new Random().Next(1, 10);
         System.Console.Write(array[i, j] + " ");
-        int temp = array[i, j];
-        if (array[i, j] < temp)
-        {
-            min = temp;
-            point[i,j] = array[i, j];
-            System.Console.WriteLine(point[i,j]);
-        }
-
     }
     System.Console.WriteLine("");
+}
+int min = array[0, 0];
 
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (array[i, j] < min)
+        {
+            min = array[i, j];
+            minRow = i;
+            minCol = j;
+        }
+    }
+}
+System.Console.WriteLine($"Минимальное значение в массиве: {min} [" + minRow + ", " + minCol + "] ");
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (array[minRow, j] != array[i, j] && array[i, minCol] != array[i, j])
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+    }
+    System.Console.WriteLine("");
 }
